1ca56926e44e4aa393a6ac9f12674f0022945561
kinfkong <kinfkong@126.com>
2020-08-05 21:22:07 +0800

    fixes

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # ikatago-client
+这是连接ikatago-server （比如百度aistudio)的客户端。
+
+暂时没时间写文档，有问题加群: 703409387
+
+## 准备
+1. 确保百度aistudio里的ikatago-server处于运行状态。  
+   （看不懂这句话啥意思的点这里: https://aistudio.baidu.com/aistudio/projectdetail/681506?shared=1)
+
+## 下载ikagato客户端
+
+Windows 64bit版本下载
+Linux版本下载
+Mac OSX版本下载
+Windows 32bit版本下载 (不要下载这个，除非你真的系统是32bit)
+
+## 用法 
+
+### Lizzie用法 
+```
+<ikatago程序路径> --platform aistudio --user <你设置的用户名> --password <你设置的密码>
+```
+比如，Windows下可能是这样子:
+```
+C:\xxx\ikatago.exe --platform aistudio --user kinfkong --password ******
+```
+
+### Sabaki的用法
+有三行，
+第一行: 引擎名字，随便起一个名字
+第二行: 程序路径，就是ikatago在你本机的路径，比如, C:\xxx\ikatago.exe
+第三行: 运行参数: --platform aistudio --user <你设置的用户名> --password <你设置的密码>
+
---
+++ package.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+CLI_VERSION=1.0.0
+rm -rf ./bin && mkdir ./bin
+
+
+# Mac OSX
+OUTPUT_PATH=ikatago-$CLI_VERSION-mac-osx
+mkdir -p ./bin/$OUTPUT_PATH
+GOOS=darwin GOARCH=amd64 go build -o ./bin/$OUTPUT_PATH/ikatago
+cd ./bin
+zip -r $OUTPUT_PATH.zip $OUTPUT_PATH
+cd - >/dev/null
+
+# linux
+OUTPUT_PATH=ikatago-$CLI_VERSION-linux
+mkdir -p ./bin/$OUTPUT_PATH
+GOOS=linux GOARCH=amd64 go build -o ./bin/$OUTPUT_PATH/ikatago
+cd ./bin
+zip -r $OUTPUT_PATH.zip $OUTPUT_PATH
+cd - >/dev/null
+
+# windows 64bit
+OUTPUT_PATH=ikatago-$CLI_VERSION-win64
+mkdir -p ./bin/$OUTPUT_PATH
+GOOS=windows GOARCH=amd64 go build -o ./bin/$OUTPUT_PATH/ikatago.exe
+cd ./bin
+zip -r $OUTPUT_PATH.zip $OUTPUT_PATH
+cd - >/dev/null
+
+# windows 32bit
+OUTPUT_PATH=ikatago-$CLI_VERSION-win32
+mkdir -p ./bin/$OUTPUT_PATH
+GOOS=windows GOARCH=386 go build -o ./bin/$OUTPUT_PATH/ikatago.exe
+cd ./bin
+zip -r $OUTPUT_PATH.zip $OUTPUT_PATH
+cd - >/dev/null
