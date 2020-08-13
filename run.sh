f4157930163c224bf4617fcf73fb473020455f34
kinfkong <kinfkong@126.com>
2020-08-14 06:27:05 +0800

    for gzip

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go run main.go --platform aistudio --username kinfkong --password 12345678 --kata-weight 20b -- version
+go run main.go --platform aistudio --username kinfkong1 --password 12345678 --kata-weight 20b -- selfplay -output-dir /tmp -models-dir /tmp -config a
