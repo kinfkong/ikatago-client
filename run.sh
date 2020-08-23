30a7a16d19ece1895dba40b95c4cef5a731ffb63
kinfkong <kinfkong@126.com>
2020-08-23 16:42:58 +0800

    fixes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-go run main.go --platform aistudio --username kktest --password 12345678 --kata-weight 20b
+go run main.go --platform aistudio --username kktest --password 12345678 --kata-weight 20b 
 #  --kata-local-config /Users/wangjinggang/gochess/remote/katago_500v.cfg
