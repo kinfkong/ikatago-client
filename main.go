1349534a752dd7306a5c1dc1e27606e85e66f55b
kinfkong <kinfkong@126.com>
2020-08-24 09:16:49 +0800

    update

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	AppVersion = "1.3.0"
+	AppVersion = "1.3.1"
 )
 
 var opts struct {
--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CLI_VERSION=1.3.0
+CLI_VERSION=1.3.1
 rm -rf ./bin && mkdir ./bin
 
 
