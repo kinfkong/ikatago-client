dbb097fd443981b5e77630b15a70c55eb3c9bb61
Jinggang Wang <kinfkong@126.com>
2022-01-13 00:14:15 +0800

    fix bugs

--- run2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# go run main.go  --platform all --username testkatago --password 12345678 --engine-type gomoku 
-#go run main.go  --platform all --username testkatago --password 12345678 --engine-type gomoku --cmd query-server
-# go run main.go --platform all --username zz-go1 --password 834173 --cmd query-server
-go run main.go --platform all --username local-go2 --password 12345678 --gpu-type share --token c76tn9gsmpvasg9sk1tg --kata-weight 20b
