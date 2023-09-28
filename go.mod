fb3c12060fe70121ba35f423fe4afc8da7ca2921
Jinggang Wang <kinfkong@126.com>
2023-09-28 09:12:35 +0800

    build: fix ios sdk

--- go.mod
+++ go.mod
@@ -1,11 +1,17 @@
 module github.com/kinfkong/ikatago-client
 
-go 1.14
+go 1.20
 
 require (
 	github.com/jessevdk/go-flags v1.4.0
-	github.com/smartystreets/goconvey v1.7.2 // indirect
 	golang.org/x/crypto v0.13.0
-	golang.org/x/mobile v0.0.0-20230922142353-e2f452493d57 // indirect
 	moul.io/http2curl v1.0.0
+)
+
+require (
+	github.com/smartystreets/goconvey v1.7.2 // indirect
+	golang.org/x/mobile v0.0.0-20230922142353-e2f452493d57 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
 )
--- ikatagosdk/build.sh
+++ ikatagosdk/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 gomobile init
-gomobile bind --target=ios -o ../../ikatago-ios-sdk/ikatagosdk.framework .
-rm -rf ~/gochess/react-native-ikatago-sdk/ios/ikatagosdk.framework
-gcp -RP ../../ikatago-ios-sdk/ikatagosdk.framework ~/gochess/react-native-ikatago-sdk/ios
+gomobile bind --target=ios -o ../../ikatago-ios-sdk/ikatagosdk.xcframework .
+rm -rf ~/gochess/react-native-ikatago-sdk/ios/ikatagosdk.xcframework
+gcp -RP ../../ikatago-ios-sdk/ikatagosdk.xcframework ~/gochess/react-native-ikatago-sdk/ios
+
 gomobile bind --target=android -o ../../ikatago-android-sdk/ikatagosdk.aar .
 gcp -RP ../../ikatago-android-sdk/ikatagosdk.aar ~/gochess/react-native-ikatago-sdk/android/libs
